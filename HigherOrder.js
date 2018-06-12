f36d712e9031f975ab1bcc7591d741abbc47b73f
Kaitlin Bunn <kabunn1991@gmail.com>
2018-06-12 18:20:26 -0400

    I can't believe we figured out the turtles

--- HigherOrder.js
+++ HigherOrder.js
@@ -48,22 +48,52 @@
 
 // 3. Functions as Return values
 
-const hazardWarningCreator = function(typeOfWarning) {
-    let warningCounter = 0;
-    return function(location) {
-        warningCounter++;
-        console.log(`"DANGER! There is a ${typeOfWarning} hazard at ${location}!"`);
-        console.log(`"The ${typeOfWarning} hazard alert has triggered ${warningCounter} time(s) today!"`);
+// const hazardWarningCreator = function(typeOfWarning) {
+//     let warningCounter = 0;
+//     return function(location) {
+//         warningCounter++;
+//         console.log(`"DANGER! There is a ${typeOfWarning} hazard at ${location}!"`);
+//         console.log(`"The ${typeOfWarning} hazard alert has triggered ${warningCounter} time(s) today!"`);
+//     }
+// }
+
+// const rocksWarning = hazardWarningCreator('Rocks on the Road');
+// const heavyRain = hazardWarningCreator('Heavy rain on the highway')
+// const realHot = hazardWarningCreator('2 hot, 2 hot');
+
+// rocksWarning('Main St and Pacific Ave');
+// rocksWarning('Times Square');
+// heavyRain('New Orleans');
+// heavyRain('Portland');
+// realHot('Atlanta');
+// realHot('Miami')
+
+
+//4. Turtles
+
+// const turtleSteps = [[0, 0], [0, 5], [-1, -3], [-3, 1], [2, -4], [3, 2]];
+
+// let positiveSteps = turtleSteps.filter(num => num[0] > -1 && num[1] > -1);
+// console.log(positiveSteps);
+// let addedSteps = positiveSteps.map(num => num[0] + num[1]);
+// console.log(addedSteps);
+// let sepSteps = addedSteps.forEach(num => console.log(num));
+
+//5. Decoding
+
+let words = 'noggin oreo the moon time tele steed his tent apollo her lives though shoo tofu budapest';
+let wordsArr = words.split(" ");
+
+const decoded = function(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let result = [];
+        if (arr[i].length === 3) {
+            result.push(' ');
+        } else {
+            result.push(arr[i].charAt(arr[i].length-1).toUpperCase());
+        }
+        console.log(result.reduce());
     }
-}
+};
 
-const rocksWarning = hazardWarningCreator('Rocks on the Road');
-const heavyRain = hazardWarningCreator('Heavy rain on the highway')
-const realHot = hazardWarningCreator('2 hot, 2 hot');
-
-rocksWarning('Main St and Pacific Ave');
-rocksWarning('Times Square');
-heavyRain('New Orleans');
-heavyRain('Portland');
-realHot('Atlanta');
-realHot('Miami')
+decoded(wordsArr);
