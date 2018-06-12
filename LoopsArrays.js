9e4361c1e3d2eb5358c14acaaea6aa060f68778d
Kaitlin Bunn <kabunn1991@gmail.com>
2018-06-12 15:24:32 -0400

    Completed average drill

--- LoopsArrays.js
+++ LoopsArrays.js
@@ -1,3 +1,7 @@
+//1. Min and Max Numbers
+
+'use strict';
+
 function max(numbers) {
     if (numbers.length === 0) {
         return undefined
@@ -89,6 +93,72 @@ function max(numbers) {
       testEmpty(min),
     ];
   
+    const numPassing = testResults.filter(function(result) {
+      return result;
+    }).length;
+    console.log(numPassing + ' out of ' + testResults.length + ' tests passing.');
+  })();
+
+  
+
+
+
+
+
+
+
+
+//2. Compute the Average
+
+function average(numbers) {
+    // for each -> add each to the previous
+    let numSum = 0;
+    numbers.forEach(function(element) {
+        numSum = element + numSum;
+    });
+    // divide this by numbers.length
+    return numSum/numbers.length;
+  }
+  
+  /* From here down, you are not expected to 
+     understand.... for now :)  
+     
+     
+     Nothing to see here!
+     
+  */
+  
+  // tests
+  
+  function testFunctionWorks(fn, input, expected) {
+    if (fn(input) === expected) {
+      console.log('SUCCESS: `' + fn.name + '` works on `[' + input + ']`');
+      return true;
+    } else {
+      console.log(
+        'FAILURE: `' +
+          fn.name +
+          '([' +
+          input +
+          '])` should be ' +
+          expected +
+          ' but was ' +
+          fn(input)
+      );
+      return false;
+    }
+  }
+  
+  (function runTests() {
+    const numList1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const correctAns1 = 5.5;
+    const numList2 = [0, -1, 1];
+    const correctAns2 = 0;
+  
+    const testResults = [
+      testFunctionWorks(average, numList1, correctAns1),
+      testFunctionWorks(average, numList2, correctAns2),
+    ];
     const numPassing = testResults.filter(function(result) {
       return result;
     }).length;
